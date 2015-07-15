93efbd06f61aa0701ba8638b5439410b6fe56500
raviqqe <raviqqe@gmail.com>
2015-07-15 02:26:44 +0000

    bug that the flag of initial nodes on memory in config.h is too large fixed

--- src/include/config.h
+++ src/include/config.h
@@ -4,8 +4,8 @@
 
 #define config_DEBUG 1
 #define config_WORD_SIZE 8 // in bytes
-#define config_MAX_NODE_MEMORY_CAPACITY 42424242
-#define config_INITIAL_NODE_MEMORY_CAPACITY 42424242
+#define config_MAX_NODE_MEMORY_CAPACITY 424242
+#define config_INITIAL_NODE_MEMORY_CAPACITY 42
 
 
 #endif

a7f1614bc47368d58a6c54ebf9743164f5427982
raviqqe <raviqqe@gmail.com>
2015-08-01 18:06:57 +0000

    main source

--- source/s9_main.c
+++ source/s9_main.c
@@ -33,7 +33,7 @@ s9_process_tokens(s9_node_memory_t * const node_memory)
       s9_debug_log(S9_DEBUG_LEVEL_VERBOSE, "a token is received.");
 
       s9_node_t *node = NULL;
-      error = s9_memory_t_gets9_node_tOfId(memory, locals9_node_tId, &node);
+      error = s9_memory_t_gets9_node_tOfId(node_memory, locals9_node_tId, &node);
       if (error) {
         s9_log(S9_LOG_LEVEL_DEBUG, "Failed to get an address of node from a node "
                       "memory.");
@@ -73,51 +73,54 @@ main(const int argc, const char * const * const argv)
   s9_error_t error = S9_OK;
 
   char *program_filename = NULL;
-  if (argc == 2) {
-    program_filename = argv[1];
+  if (argc == 3) {
+    node_filename = argv[1];
+    token_filename = argv[2];
   } else {
-    s9_log(S9_LOG_LEVEL_NOTICE, "Usage: %s <program file>", argv[0]);
+    s9_log(S9_LOG_LEVEL_NOTICE, "usage: %s <node file> <token file>", argv[0]);
     error = S9_ERROR_COMMAND_ARGS;
     goto just_exit;
   }
 
-  error = s9_initialize_network();
+  s9_protocol_t protocol = S9_DUMMY_PROTOCOL;
+  error = s9_initialize_network(&protocol);
   if (error) {
-    s9_log(S9_LOG_LEVEL_DEBUG, "Failed to initialize communication environment of "
-                  "processors.");
+    s9_debug_log(S9_DEBUG_LEVEL_ERROR,
+                 "failed to initialize the processor network.");
     goto just_exit;
   }
 
-  s9_log(S9_LOG_LEVEL_DEBUG, "A processor launched!");
+  s9_debug_log(S9_DEBUG_LEVEL_VERBOSE,
+               "the processor #%lld launched!", protocol.processor_id);
 
-  if (s9_is_master_processor()) {
-    error = s9_load_program(programFileName);
-    if (error) {
-      s9_log(S9_LOG_LEVEL_DEBUG, "Failed to load a program to processors.");
-      goto finalize_network;
-    }
-  }
-
-  s9_memory_t memory = S9_DUMMY_MEMORY;
-  error = s9_initilalize_memory(&memory);
+  s9_node_memory_t node_memory = S9_DUMMY_NODE_MEMORY;
+  error = s9_initilalize_node_memory(&node_memory);
   if (error) {
-    s9_log(S9_LOG_LEVEL_DEBUG, "Failed to initialize a node memory.");
+    s9_debug_log(S9_DEBUG_LEVEL_ERROR, "failed to initialize a node memory.");
     goto finalize_network;
   }
-  s9_log(S9_LOG_LEVEL_DEBUG, "s9_memory_t initialized!");
 
-  error = s9_process_messages(&memory);
+  error = s9_load_nodes(node_file, &node_memory, protocol);
+  if (error) {
+    s9_debug_log(S9_DEBUG_LEVEL_ERROR,
+                 "failed to load nodes to a node memory.");
+    goto finalize_node_memory;
+  }
+
+  s9_debug_log(S9_DEBUG_LEVEL_VERBOSE, "a node memory initialized!");
+
+  error = s9_process_messages(&node_memory);
   if (error) {
     s9_log(S9_LOG_LEVEL_DEBUG, "Failed to processs some messages.");
-    goto finalize_memory;
+    goto finalize_node_memory;
   }
 
   s9_error_t error_on_finalization = S9_OK;
-finalize_memory:
-  error_on_finalization = s9_finalize_memory(&memory);
+finalize_node_memory:
+  error_on_finalization = s9_finalize_node_memory(&node_memory);
   if (error_on_finalization) {
     s9_log(S9_LOG_LEVEL_DEBUG,
-           "Failed to finalize a memory. "
+           "failed to finalize a node memory. "
            "(error code: %d)", error_on_finalization);
     goto finalize_network;
   }
@@ -127,19 +130,20 @@ finalize_network:
     error_on_finalization = s9_send_message(
         s9_message_of_signal(S9_SIGNAL_SHUTDOWN));
     if (error_on_finalization) {
-      s9_log(S9_LOG_LEVEL_DEBUG,
-             "failed to send shutdown signals to processors. "
-             "(error code: %d)", error_on_finalization);
+      s9_debug_log(S9_LOG_LEVEL_ERROR,
+                   "failed to send shutdown signals to processors. "
+                   "(error code: %d)", error_on_finalization);
     } else {
-      s9_log(S9_LOG_LEVEL_DEBUG, "shutdown signal sent to all processors.");
+      s9_debug_log(S9_LOG_LEVEL_VERBOSE,
+                   "shutdown signal sent to all processors.");
     }
   }
 
-  error_on_finalizatoin = s9_finalize_network();
+  error_on_finalization = s9_finalize_network();
   if (error_on_finalization) {
-    s9_log(S9_LOG_LEVEL_DEBUG,
-           "failed to finalize the processor network. "
-           "(error code: %d)", error_on_finalization);
+    s9_debug_log(S9_LOG_LEVEL_ERROR,
+                 "failed to finalize the processor network. "
+                 "(error code: %d)", error_on_finalization);
     goto just_exit;
   }
 
